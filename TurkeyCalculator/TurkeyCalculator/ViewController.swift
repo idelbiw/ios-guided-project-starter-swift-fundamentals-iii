6ff9e583ea1a40fd1358170377f8fe1a79608895
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-01-23 21:05:07 -0500

    all done, also added a label to show the cook time in hours

--- TurkeyCalculator/TurkeyCalculator/ViewController.swift
+++ TurkeyCalculator/TurkeyCalculator/ViewController.swift
@@ -15,10 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet var turkeyWeightTextField: UITextField!
     @IBOutlet var cookTimeTextField: UITextField!
     @IBOutlet var metricButton: UIButton!
+    @IBOutlet var cookTimeInHoursLabel: UILabel!
     
     // Actions
     
     @IBAction func calculateButtonPressed(_ sender: Any) {
+        guard let unwrappedTurkeyWeight = turkeyWeightTextField.text else {
+            cookTimeTextField.text = "Nothing was entered"
+            print("Nothing was entered")
+            return}
+        guard let turkeyWeight = Double(unwrappedTurkeyWeight) else {
+            cookTimeTextField.text = "invalid amount entered"
+            cookTimeInHoursLabel.text = ""
+            print("invalid amount entered")
+            return}
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = 2
+        
+        let result = turkeyWeight * 20
+        let resultInHours = result / 60
+        let hourString = numberFormatter.string(for: resultInHours)!
+        cookTimeTextField.text = "\(result) minutes"
+        cookTimeInHoursLabel.text = "\(hourString) hours"
+        
         
     }
     
@@ -27,4 +47,6 @@ class ViewController: UIViewController {
     }
     
     // Helper functions
+    
+    
 }
